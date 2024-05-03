4abb8369d953ba74853d653f14265fcdfef64df7
Jayanth  Challapalli <jayanth.challapalli@kellogg.northwestern.edu>
2024-05-03 04:40:45 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -12,21 +12,30 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_03_042754) do
   create_table "actors", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "year_released"
+    t.string "rated"
+    t.integer "studio_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "roles", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "actor_id"
+    t.string "character_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "studios", force: :cascade do |t|
+    t.string "Studio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
